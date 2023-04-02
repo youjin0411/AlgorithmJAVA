ad28e80d93d9fde54a49986f80928fb9e295efaf
Kim-yujin <w2117@e-mirim.hs.kr>
2023-04-02 17:14:08 +0900

    02. 대소문자 변환

---
+++ CaseConversion.java
@@ -0,0 +1,28 @@
+// 02. 대소문자 변환
+//대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로
+// 변환하여 출력하는 프로그램을 작성하세요.
+import java.util.*;
+public class CaseConversion {
+    public String solution(String str){
+        String answer = "";
+        for(char x: str.toCharArray()){
+            if(Character.isLowerCase(x)) answer += Character.toUpperCase(x);
+            else answer += Character.toUpperCase(x);
+        }
+        return answer;
+    }
+//public String solution(String str){
+//    String answer="";
+//    for(char x : str.toCharArray()){
+//        if(x>=97 && x<=122) answer+=(char)(x-32); //문자 - 32는 대문자
+//        else answer+=(char)(x+32); //문자 + 32는 소문자
+//    }
+//    return answer;
+//}
+    public static void main(String[] args){
+        CaseConversion T = new CaseConversion();
+        Scanner kb = new Scanner(System.in);
+        String str=kb.next();
+        System.out.print(T.solution(str));
+    }
+}
