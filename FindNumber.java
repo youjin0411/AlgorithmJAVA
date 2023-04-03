03424b21183270543a717e050bbe7e90b190ef44
Kim-yujin <w2117@e-mirim.hs.kr>
2023-04-03 20:05:30 +0900

    09. 숫자만 추출 10. 가장 짧은 문자거리

---
+++ FindNumber.java
@@ -0,0 +1,26 @@
+// 09. 숫자만 추출
+//문자와 숫자가 섞여있는 문자열이 주어지면 그 중 숫자만 추출하여 그 순서대로 자연수를 만듭니다.
+//만약 “tge0a1h205er”에서 숫자만 추출하면 0, 1, 2, 0, 5이고 이것을 자연수를 만들면 1205이 됩니다.
+//추출하여 만들어지는 자연수는 100,000,000을 넘지 않습니다.
+
+import java.util.*;
+public class FindNumber {
+    public int solution(String str){
+        int num = 0;
+        String s = "";
+        char[] c = str.toCharArray();
+        for(char x : c){
+            if(!Character.isAlphabetic(x)){
+                s += x;
+            }
+        }
+        num = Integer.parseInt(s);
+        return num;
+    }
+    public static void main(String[] args){
+        FindNumber T = new FindNumber();
+        Scanner kb = new Scanner(System.in);
+        String str=kb.next();
+        System.out.print(T.solution(str));
+    }
+}
---
+++ MostShortCharDistance.java
@@ -0,0 +1,39 @@
+// 10. 가장 짧은 문자거리
+// 개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 문자 t와 떨어진
+// 최소거리를 출력하는 프로그램을 작성하세요.
+import java.util.*;
+public class MostShortCharDistance {
+    public int[] solution(String s, char t){
+        int[] answer=new int[s.length()];
+        int p=1000;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)==t){
+                p=0;
+                answer[i]=p;
+            }
+            else{
+                p++;
+                answer[i]=p;
+            }
+        }
+        p=1000;
+        for(int i=s.length()-1; i>=0; i--){
+            if(s.charAt(i)==t) p=0;
+            else{
+                p++;
+                answer[i]=Math.min(answer[i], p);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        MostShortCharDistance T = new MostShortCharDistance();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+        for (int x : T.solution(str, c)) {
+            System.out.print(x + " ");
+        }
+    }
+}
